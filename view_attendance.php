9e208707417fc5e0fd68fd8746a96dc4ffe55891
adnan-mohsen <majesty1433@gmail.com>
2026-03-28 12:08:21 +0300

    Update view_attendance.php

--- view_attendance.php
+++ view_attendance.php
@@ -1,3 +1,4 @@
+<?php include 'check_login.php'; ?>
 <div style="text-align: left; margin-bottom: 10px;">
     <button onclick="window.print()" class="btn-filter" style="background-color: #28a745;">🖨️ طباعة التقرير (PDF)</button>
 </div>
