0ecfbf693732dff68c8e074ba2e59e5fcc11c005
adnan-mohsen <majesty1433@gmail.com>
2026-03-28 12:07:23 +0300

    Update employees.php

--- employees.php
+++ employees.php
@@ -1,3 +1,4 @@
+<?php include 'check_login.php'; ?>
 <div style="text-align: center; margin-bottom: 20px;">
     <a href="take_attendance.php" class="btn" style="background-color: #28a745;">📝 تسجيل حضور اليوم</a>
     <a href="view_attendance.php" class="btn" style="background-color: #6f42c1;">📊 عرض التقارير</a>
